33fa38fe66e3d8e456aaf7ac362934030a03df0b
Yojitha-10 <pabbisettyyojitha@gmail.com>
2024-06-18 17:34:41 +0530

    day2assignments
    day2assignments

---
+++ day2assignments/Action.java
@@ -0,0 +1,14 @@
+package day2assignments;
+
+class Action extends Movie {
+    // Constructor
+    public Action(String title, int idNumber, String mpaaRating) {
+        super(title, idNumber, mpaaRating);
+    }
+ 
+    // Overriding calcLateFees method
+    @Override
+    public double calcLateFees(int daysLate) {
+        return daysLate * 3.0;
+    }
+}
---
+++ day2assignments/Comedy.java
@@ -0,0 +1,14 @@
+package day2assignments;
+
+class Comedy extends Movie {
+    // Constructor
+    public Comedy(String title, int idNumber, String mpaaRating) {
+        super(title, idNumber, mpaaRating);
+    }
+ 
+    // Overriding calcLateFees method
+    @Override
+    public double calcLateFees(int daysLate) {
+        return daysLate * 2.5;
+    }
+}
